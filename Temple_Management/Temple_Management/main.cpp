db353426b8fd5da34ffcbe7c281112c3a782127e
guatn12 <guatn12@gmail.com>
2015-09-23 19:59:38 +0900

    1Day

--- Temple_Management/Temple_Management/main.cpp
+++ Temple_Management/Temple_Management/main.cpp
@@ -1,5 +1,5 @@
 #include<Windows.h>
-
+#include "resource.h"
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 HINSTANCE g_hInst;
@@ -20,7 +20,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	WndClass.hInstance = hInstance;
 	WndClass.lpfnWndProc = WndProc;
 	WndClass.lpszClassName = lpszClass;
-	WndClass.lpszMenuName = NULL;
+	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
 	WndClass.style = CS_HREDRAW | CS_VREDRAW;
 	RegisterClass(&WndClass);
 
@@ -37,7 +37,29 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
+	int ChoseExitOrSave;
+
 	switch (iMessage){
+	case WM_CREATE:
+		return 0;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)){
+		case ID_Believer:
+			break;
+		case ID_Event:
+			break;
+		case ID_Print:
+			break;
+		case ID_Exit:
+			ChoseExitOrSave = MessageBox(hWnd, TEXT("입력된 정보를 저장 후 종료 하시겠습니까?"), TEXT("종료"), MB_OKCANCEL);
+			if (ChoseExitOrSave == IDCANCEL)
+				break;
+			PostQuitMessage(0);
+			return 0;
+		case ID_INF:
+			break;
+		}
+		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
